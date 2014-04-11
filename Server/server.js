d05921d9db1e76f2f155434631e355037d76d03f
Taylor Brazelton <ryanb58@live.com>
2014-04-10 21:59:04 -0400

    Got socket working. Fundamentally understand!

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,27 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(80);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+	socket.on('sayHello', function (data) {
+    console.log(data);
+  });
+});
